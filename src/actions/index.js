d5c6c8f89dd6a12094a40b67b5893cab0ea26d41
deivaotv <xdeividsabinox@live.com>
2021-01-22 20:05:14 -0300

    alteração redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,10 @@ export const DARKMODE_UPDATE = value => ({
     type: 'DARKMODE_UPDATE',
     checked: value,
 });
+
+export const setDarkMode = (checked) => ({
+    type: 'DARKMODE_UPDATE',
+    payload: {
+        checked
+    }
+});
